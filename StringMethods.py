1cee2682ab978fa72a1737767f57b48fd20fe911
arpit-aka-appu <apple@Apples-MacBook-Pro.local>
2026-09-02 23:41:34 +0530

    String Methods

---
+++ StringMethods.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+print (name.lower())
+print (name.upper())
+print (name.title())
+print (name.find("A"))
+print (name.replace("patel", "kumar"))
+print(name.count("a"))
